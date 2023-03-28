17dee470b28ff69ed88099413d8722642cb3caf6
Zárai László Levente <zarai.laszlo.levente@gmail.com>
2023-03-28 16:08:31 +0200

    Add project files.

---
+++ S1EORI_HFT_2022232/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S1EORI_HFT_2022232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
