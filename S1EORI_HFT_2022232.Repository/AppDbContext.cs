af952abd0b41139c0fad86a685b779ecec560b45
Zárai László Levente <zarai.laszlo.levente@gmail.com>
2023-05-22 20:55:24 +0200

    DbContext Class Created

---
+++ S1EORI_HFT_2022232.Repository/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S1EORI_HFT_2022232.Models;
+using System;
+
+namespace S1EORI_HFT_2022232.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<GitRepository> GitRepositorys { get; set; }
+        public DbSet<Commit> Commits { get; set; }
+        public DbSet<Milestone> Milestones { get; set; }
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseInMemoryDatabase("DatabaseGit")
+                .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
