94e24eae91be155ca0c70378ba4a75f115b38439
Zárai László Levente <zarai.laszlo.levente@gmail.com>
2023-05-24 12:35:13 +0200

    StatController finished

--- S1EORI_HFT_2022232.Endpoint/Controllers/StatController.cs
+++ S1EORI_HFT_2022232.Endpoint/Controllers/StatController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using S1EORI_HFT_2022232.Logic.Interfaces;
+using S1EORI_HFT_2022232.Models;
 using S1EORI_HFT_2022232.Models.Statistics;
 using S1EORI_HFT_2022232.Repository;
+using System;
 using System.Collections.Generic;
 
 
@@ -12,10 +14,22 @@ namespace S1EORI_HFT_2022232.Endpoint.Controllers
     public class StatController : ControllerBase
     {
         IGitRepositoryLogic gitRepositoryLogic;
+        ICommitLogic commitLogic;
+        IUserLogic userLogic;
         public StatController(IGitRepositoryLogic gitRepositoryLogic)
         {
             this.gitRepositoryLogic = gitRepositoryLogic;
         }
+        public StatController(ICommitLogic commitLogic)
+        {
+            this.commitLogic = commitLogic;
+        }
+        public StatController(IUserLogic userLogic)
+        {
+            this.userLogic = userLogic;
+        }
+
+        //gitRepositoryLogic non curd
         [HttpGet]
         public IEnumerable<RepositoryStatistics> ReadRepositoryStats()
         {
@@ -31,6 +45,32 @@ namespace S1EORI_HFT_2022232.Endpoint.Controllers
         {
             return gitRepositoryLogic.GetCommitCountForRepository(repositoryId);            
         }
-
+        //commitLogic non crud
+        [HttpGet("{date}")]
+        public IEnumerable<Commit> GetCommitsNewerThan(DateTime date)
+        {
+            return commitLogic.GetCommitsNewerThan(date);
+        }
+        [HttpGet("{length}")]
+        public IEnumerable<Commit> ReadCommitsLongerThan(int length)
+        {
+            return commitLogic.ReadCommitsLongerThan(length);
+        }
+        //userLogic non crud
+        [HttpGet]
+        public IEnumerable<User> ReadUsersWithZeroRepositories()
+        {
+            return userLogic.ReadUsersWithZeroRepositories();
+        }
+        [HttpGet("{email}")]
+        public bool DoesUserWithEmailExist(string email)
+        {
+            return userLogic.DoesUserWithEmailExist(email);
+        }
+        [HttpGet("{age}")]
+        public IEnumerable<User> ReadUsersOlderThan(int age)
+        {
+            return userLogic.ReadUsersOlderThan(age);
+        }
     }
 }
