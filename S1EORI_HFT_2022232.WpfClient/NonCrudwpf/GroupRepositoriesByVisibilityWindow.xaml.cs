3c90d24e0c313aecfdbdde7bcd0630491b4a7b63
László Zárai <bloodyfox275@gmail.com>
2023-11-15 17:33:37 +0100

    NonCrud Windows

---
+++ S1EORI_HFT_2022232.WpfClient/NonCrudwpf/GroupRepositoriesByVisibilityWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace S1EORI_HFT_2022232.WpfClient.NonCrudwpf
+{
+    /// <summary>
+    /// Interaction logic for GroupRepositoriesByVisibilityWindow.xaml
+    /// </summary>
+    public partial class GroupRepositoriesByVisibilityWindow : Window
+    {
+        public GroupRepositoriesByVisibilityWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- S1EORI_HFT_2022232.WpfClient/ViewModels/NoNCRUDWindowViewModel.cs
+++ S1EORI_HFT_2022232.WpfClient/ViewModels/NoNCRUDWindowViewModel.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using S1EORI_HFT_2022232.WpfClient.NonCrudwpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace S1EORI_HFT_2022232.WpfClient.ViewModels
 {
-    internal class NoNCRUDWindowViewModel
+    public class NoNCRUDWindowViewModel
     {
+        public ICommand GetCommitCountForRepositoryCommand { get; }
+        public ICommand ReadRepositoryStatsCommand { get; }
+        public ICommand GroupRepositoriesByVisibilityCommand { get; }
+        public ICommand ReadUsersWithZeroRepositoriesCommand { get; }
+        public ICommand ReadUsersOlderThanCommand { get; }
+
+        public NoNCRUDWindowViewModel()
+        {
+            GetCommitCountForRepositoryCommand = new RelayCommand(
+                () => new GetCommitCountForRepositoryWindow().ShowDialog()
+                );
+
+            ReadRepositoryStatsCommand = new RelayCommand(
+                () => new ReadRepositoryStatsWindow().ShowDialog()
+                );
+
+            GroupRepositoriesByVisibilityCommand = new RelayCommand(
+                () => new GroupRepositoriesByVisibilityWindow().ShowDialog()
+                );
+
+            ReadUsersWithZeroRepositoriesCommand = new RelayCommand(
+                () => new ReadUsersWithZeroRepositoriesWindow().ShowDialog()
+                );
+
+            ReadUsersOlderThanCommand = new RelayCommand(
+                () => new ReadUsersOlderThanWindow().ShowDialog()
+                );
+        }            
     }
 }
